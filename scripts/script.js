7c80a81c3fe5957e5c63528e879468d0a1e47ce6
Nahuel <ovettini@gmail.com>
2024-06-12 18:03:02 -0300

    login

--- scripts/script.js
+++ scripts/script.js
@@ -171,11 +171,26 @@ init('card-container-2');
 document.addEventListener("DOMContentLoaded", function() {
     const userStatus = document.getElementById("userStatus");
     const storedEmail = localStorage.getItem('userEmail');
-  
+
     if (storedEmail) {
-      userStatus.innerHTML = `Bienvenido: ${storedEmail}`;
-        if(storedEmail == "zenithadmin@zenith.com")
+      userStatus.innerHTML = `Bienvenido: ${storedEmail}` 
+    //  , <button id="logout" href="#">Cerrar sesión</button>`;
+        
+      if(storedEmail == "zenithadmin@zenith.com")
             userStatus.innerHTML= `<a href="screens/productForm.html">Cargar producto</a>`
     }   else {
       userStatus.innerHTML = `<a href="screens/login.html">Iniciar sesión</a>`;
     }});
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      })
